8e03e2c5d34db8eb7316ddc0755b2123450c533f
nidys <piotr.niedrygas@gmail.com>
2014-05-11 20:30:48 +0200

    init eclipse commit

---
+++ src/client/Minesweeper.java
@@ -0,0 +1,13 @@
+package client;
+
+public class Minesweeper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Client started...");
+	}
+
+}
