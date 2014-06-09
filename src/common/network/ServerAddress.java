eacefcf64b766847e623538f3e0159881c501c32
nidys <piotr.niedrygas@gmail.com>
2014-06-09 22:26:02 +0200

    small refactor - server addr

--- src/common/network/ServerAddress.java
+++ src/common/network/ServerAddress.java
@@ -1,14 +1,31 @@
 package common.network;
 
 public enum ServerAddress {
-	LOCALHOST("127.0.0.1:1099");
+	LOCALHOST("127.0.0.1", Port.DEFAULT);
 	private String addr;
 
-	private ServerAddress(String s) {
-		addr = s;
+	private ServerAddress(String addr, Port p) {
+		addr = addr + ":" + p.getValue();
 	}
 
 	public String getValue() {
 		return addr;
+	}
+
+	public enum Port {
+		DEFAULT("1099");
+		private String port;
+
+		private Port(String s) {
+			port = s;
+		}
+
+		public String getValue() {
+			return port;
+		}
+
+		public int getInt() {
+			return Integer.valueOf(port);
+		}
 	}
 }
--- src/server/RoomManager.java
+++ src/server/RoomManager.java
@@ -8,6 +8,7 @@ import org.apache.log4j.Logger;
 
 import common.network.GameManager;
 import common.network.ServerAddress;
+import common.network.ServerAddress.Port;
 
 public class RoomManager {
 	private static Logger log = Logger.getLogger(RoomManager.class);
@@ -17,7 +18,7 @@ public class RoomManager {
 	 */
 	public static void main(String[] args) {
 		try {
-			LocateRegistry.createRegistry(1099);
+			LocateRegistry.createRegistry(Port.DEFAULT.getInt());
 			GameManager gs = new GameManagerImpl();
 			UnicastRemoteObject.exportObject(gs, 0);
 
