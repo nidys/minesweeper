2eeb360c2fe88aedd88bb9d986107ce163529c7d
aemulor <ardoris3@gmail.com>
2014-06-17 23:29:11 +0200

    PlayerGameBoardPanel: Panel resizes to content.

--- src/client/internationalization/DialogText.java
+++ src/client/internationalization/DialogText.java
@@ -4,4 +4,8 @@ public class DialogText {
 	public static final String END_GAME_TITLE = "You lost!";
 	public static final String END_GAME_MESSAGE = "You lost :(.\n Start again?";
 	public static final String NEW_GAME_TITLE = "Minesweeper: New game";
+	public static final String MAINWINDOW_CLASSIC_TITLE = "Minesweeper: CLASSIC";
+	public static final String MAINWINDOW_SHARED_TITLE = "Minesweeper: SHARED";
+	public static final String MAINWINDOW_PERKS_TITLE = "Minesweeper: PERKS";
+	
 }
--- src/client/views/MainWindow.java
+++ src/client/views/MainWindow.java
@@ -2,6 +2,7 @@ package client.views;
 
 import static client.internationalization.ButtonNames.NEW_GAME;
 
+import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
@@ -26,7 +27,6 @@ import client.internationalization.ButtonNames;
 import client.internationalization.DialogText;
 import client.utils.GraphicsFactory;
 import client.views.component.PlayerGameBoardPanel;
-
 import common.enums.GameMode;
 import common.network.ServerAddress;
 
@@ -37,21 +37,22 @@ import common.network.ServerAddress;
 public class MainWindow extends WindowBase {
 	private static Logger log = Logger.getLogger(MainWindow.class);
 	private static GraphicsFactory graphicsFactory = new GraphicsFactory();
-	
+
 	private JButton newGameBtn;
 	private JButton btnReset;
 	private JTextField serverAddress;
 	private JTextField userNick;
-	
+
 	private GamePanelBase gamePanel;
 	/**
 	 * Create the frame.
 	 */
 	public MainWindow() {
-		setIconImage(Toolkit.getDefaultToolkit().getImage(MainWindow.class.getResource("/resources/images/bomb.png")));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(
+				MainWindow.class.getResource("/resources/images/bomb.png")));
 		setResizable(false);
 		drawEntryScreen();
-		//drawGameBoard();
+		// drawGameBoard();
 	}
 
 	public void drawGameBoard() {
@@ -68,29 +69,31 @@ public class MainWindow extends WindowBase {
 		setBounds(100, 100, 456, 356);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new GridLayout(0, 1, 0, 0));
-		
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-			
-						btnReset = new JButton(ButtonNames.RESET);
-						menuBar.add(btnReset);
-						getContentPane().setBounds(0,0,450,301);
-						this.setResizable(true);
-	}
 
+		btnReset = new JButton(ButtonNames.RESET);
+		menuBar.add(btnReset);
+		getContentPane().setBounds(0, 0, 450, 301);
+		this.setResizable(true);
+	}
 
 	private void drawEntryScreen() {
 		setBounds(100, 100, 463, 288);
 		this.setResizable(false);
-		
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{43, 1, 55, 86, 31, 41, 0, 0, 0, 0};
+		gridBagLayout.columnWidths = new int[]{43, 1, 55, 86, 31, 41, 0, 0, 0,
+				0};
 		gridBagLayout.rowHeights = new int[]{34, 1, 36, 29, 20, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0,
+				0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 1.0,
+				Double.MIN_VALUE};
 		getContentPane().setLayout(gridBagLayout);
-		
+
 		JLabel lblMinesweeper = new JLabel(graphicsFactory.getLogoIcon());
 		lblMinesweeper.setText("");
 		lblMinesweeper.setFont(new Font("Tahoma", Font.PLAIN, 56));
@@ -101,7 +104,7 @@ public class MainWindow extends WindowBase {
 		gbc_lblMinesweeper.gridx = 1;
 		gbc_lblMinesweeper.gridy = 1;
 		getContentPane().add(lblMinesweeper, gbc_lblMinesweeper);
-		
+
 		JLabel lblServerAdress = new JLabel("Server adress:");
 		GridBagConstraints gbc_lblServerAdress = new GridBagConstraints();
 		gbc_lblServerAdress.fill = GridBagConstraints.VERTICAL;
@@ -111,29 +114,29 @@ public class MainWindow extends WindowBase {
 		gbc_lblServerAdress.gridx = 1;
 		gbc_lblServerAdress.gridy = 3;
 		getContentPane().add(lblServerAdress, gbc_lblServerAdress);
-		
-				serverAddress = new JTextField();
-				serverAddress.setText(ServerAddress.LOCALHOST.getValue());
-				serverAddress.setColumns(10);
-				GridBagConstraints gbc_serverAddress = new GridBagConstraints();
-				gbc_serverAddress.fill = GridBagConstraints.BOTH;
-				gbc_serverAddress.insets = new Insets(0, 0, 5, 5);
-				gbc_serverAddress.gridx = 3;
-				gbc_serverAddress.gridy = 3;
-				getContentPane().add(serverAddress, gbc_serverAddress);
-		
-				newGameBtn = new JButton(NEW_GAME);
-				newGameBtn.setBackground(UIManager.getColor("Button.background"));
-				GridBagConstraints gbc_newGameBtn = new GridBagConstraints();
-				gbc_newGameBtn.fill = GridBagConstraints.VERTICAL;
-				gbc_newGameBtn.gridwidth = 2;
-				gbc_newGameBtn.gridheight = 2;
-				gbc_newGameBtn.anchor = GridBagConstraints.WEST;
-				gbc_newGameBtn.insets = new Insets(0, 0, 5, 5);
-				gbc_newGameBtn.gridx = 6;
-				gbc_newGameBtn.gridy = 3;
-				getContentPane().add(newGameBtn, gbc_newGameBtn);
-		
+
+		serverAddress = new JTextField();
+		serverAddress.setText(ServerAddress.LOCALHOST.getValue());
+		serverAddress.setColumns(10);
+		GridBagConstraints gbc_serverAddress = new GridBagConstraints();
+		gbc_serverAddress.fill = GridBagConstraints.BOTH;
+		gbc_serverAddress.insets = new Insets(0, 0, 5, 5);
+		gbc_serverAddress.gridx = 3;
+		gbc_serverAddress.gridy = 3;
+		getContentPane().add(serverAddress, gbc_serverAddress);
+
+		newGameBtn = new JButton(NEW_GAME);
+		newGameBtn.setBackground(UIManager.getColor("Button.background"));
+		GridBagConstraints gbc_newGameBtn = new GridBagConstraints();
+		gbc_newGameBtn.fill = GridBagConstraints.VERTICAL;
+		gbc_newGameBtn.gridwidth = 2;
+		gbc_newGameBtn.gridheight = 2;
+		gbc_newGameBtn.anchor = GridBagConstraints.WEST;
+		gbc_newGameBtn.insets = new Insets(0, 0, 5, 5);
+		gbc_newGameBtn.gridx = 6;
+		gbc_newGameBtn.gridy = 3;
+		getContentPane().add(newGameBtn, gbc_newGameBtn);
+
 		JLabel lblUserName = new JLabel("User name:");
 		GridBagConstraints gbc_lblUserName = new GridBagConstraints();
 		gbc_lblUserName.gridwidth = 2;
@@ -142,21 +145,21 @@ public class MainWindow extends WindowBase {
 		gbc_lblUserName.gridx = 1;
 		gbc_lblUserName.gridy = 4;
 		getContentPane().add(lblUserName, gbc_lblUserName);
-		
-				userNick = new JTextField();
-				// generate random user name so that user don't need to write it all the
-				// time
-				userNick.setText("user" + String.valueOf(new Random().nextInt(100)));
-				userNick.setColumns(10);
-				GridBagConstraints gbc_userNick = new GridBagConstraints();
-				gbc_userNick.fill = GridBagConstraints.HORIZONTAL;
-				gbc_userNick.anchor = GridBagConstraints.NORTH;
-				gbc_userNick.insets = new Insets(0, 0, 5, 5);
-				gbc_userNick.gridx = 3;
-				gbc_userNick.gridy = 4;
-				getContentPane().add(userNick, gbc_userNick);
+
+		userNick = new JTextField();
+		// generate random user name so that user don't need to write it all the
+		// time
+		userNick.setText("user" + String.valueOf(new Random().nextInt(100)));
+		userNick.setColumns(10);
+		GridBagConstraints gbc_userNick = new GridBagConstraints();
+		gbc_userNick.fill = GridBagConstraints.HORIZONTAL;
+		gbc_userNick.anchor = GridBagConstraints.NORTH;
+		gbc_userNick.insets = new Insets(0, 0, 5, 5);
+		gbc_userNick.gridx = 3;
+		gbc_userNick.gridy = 4;
+		getContentPane().add(userNick, gbc_userNick);
 		getContentPane().validate();
-		
+
 	}
 
 	/**
@@ -171,16 +174,9 @@ public class MainWindow extends WindowBase {
 	public void addResetBtnListener(ActionListener listener) {
 		btnReset.addActionListener(listener);
 	}
-	
-	public void addBombFieldBtnListener(MyBombFielsBtnController listener) {
-		gamePanel.addBombFieldBtnListener(listener);
-	}
 
-
-	public void addNewPlayerToView(PlayerGameBoardPanel playerGameBoardPanel) {
-		gamePanel.addPlayer(playerGameBoardPanel);
-	}
 	
+
 	public String getServerAddress() {
 		return serverAddress.getText();
 	}
@@ -188,68 +184,67 @@ public class MainWindow extends WindowBase {
 	public String getUserNick() {
 		return userNick.getText();
 	}
-	
-
 
 	// TODO MALY COMMIT
 	public void setMyFieldAsBomb(int pos) {
 		// TODO from now shot return list of fields to discover
-		//myBombField[pos - 1].setBackground(Color.RED);
+		// myBombField[pos - 1].setBackground(Color.RED);
 	}
-	
+
 	public void initializeGameBoard(GameMode mode) {
-		if (mode == GameMode.PERKS){
+		if (mode == GameMode.PERKS) {
 			gamePanel = new PerksGamePanel();
 			getContentPane().add(gamePanel);
-		}else if (mode == GameMode.CLASSIC){
+			this.setTitle(DialogText.MAINWINDOW_PERKS_TITLE);
+		} else if (mode == GameMode.CLASSIC) {
 			// TODO AGA, for testing here
 			gamePanel = new ClassicGamePanel(100);
 			getContentPane().add(gamePanel);
-		}else if (mode == GameMode.SHARED){
-			
-		}else{
-			
+			this.setTitle(DialogText.MAINWINDOW_CLASSIC_TITLE);
+		} else if (mode == GameMode.SHARED) {
+			this.setTitle(DialogText.MAINWINDOW_SHARED_TITLE);
+		} else {
+
 		}
 	}
-	
 
-	public void setFieldAsBomb(int position) {
-		gamePanel.setFieldAsBomb(position);
 	
-		// TODO Aga Delete magic strings (use internationalization)
-		displayEndGameMessage();
-		
+	public void addBombFieldBtnListener(MyBombFielsBtnController listener) {
+		gamePanel.addBombFieldBtnListener(listener);
+	}
+
+	public void addNewPlayerToView(PlayerGameBoardPanel playerGameBoardPanel) {
+		gamePanel.addPlayer(playerGameBoardPanel);
+		Dimension d = this.getLayout().preferredLayoutSize(this);
+		this.setSize(d);
 	}
 	
+	
+	
 
-	private void displayEndGameMessage()
-	{
+	private void displayEndGameMessage() {
 		int option = JOptionPane.showConfirmDialog(this,
-			    DialogText.END_GAME_TITLE,
-			    DialogText.END_GAME_MESSAGE,
-			    JOptionPane.YES_NO_OPTION);
-		
-		if (option == 0){ //YES
-			
-		}else if (option == 1){ //NO
-			
+				DialogText.END_GAME_TITLE, DialogText.END_GAME_MESSAGE,
+				JOptionPane.YES_NO_OPTION);
+
+		if (option == 0) { // YES
+
+		} else if (option == 1) { // NO
+
 		}
-		
+
 	}
-	
-	
+
 	/**
 	 * TODO PlayerHandler.opponentShot, commented until SHARED,PERKS will be
 	 * developed
 	 */
 	// TODO MALY COMMIT
 	public void setOpponentAsBomb(int pos) {
-		//oponentBombField[pos - 1].setBackground(Color.RED);
+		// oponentBombField[pos - 1].setBackground(Color.RED);
 	}
 
-	public void setFieldAsEmpty(int position) {
-		gamePanel.setFieldAsEmpty(position);
-	}
+	
 
 	/**
 	 * TODO PlayerHandler.opponentShot, commented until SHARED,PERKS will be
@@ -257,18 +252,33 @@ public class MainWindow extends WindowBase {
 	 */
 	// TODO MALY COMMIT
 	public void setOpponentAsEmpty(int pos) {
-		//oponentBombField[pos - 1].setBackground(Color.GRAY);
+		// oponentBombField[pos - 1].setBackground(Color.GRAY);
 	}
 
 	public void resetFields() {
 		gamePanel.resetFields();
 	}
-	
+
 	public void setFieldAsFlagged(int position) {
 		gamePanel.setFieldAsFlagged(position);
 	}
+
+	public void setFieldAsEmpty(int position) {
+		gamePanel.setFieldAsEmpty(position);
+	}
 	
-	public void setFieldAsEmptyWithValue(int position, int value){
+	public void setFieldAsEmptyWithValue(int position, int value) {
 		gamePanel.setFieldAsEmptyWithValue(position, value);
+	}
+	
+	public void setFieldAsBomb(int position) {
+		gamePanel.setFieldAsBomb(position);
+
+		// TODO Aga Delete magic strings (use internationalization)
+		displayEndGameMessage();
+	}
+	
+	public void setProgress(String userNick, int progressValue){
+		gamePanel.setProgress(userNick, progressValue);
 	}
 }
